456feeb5076b4f5ab38100cf99817938fae91b48
VivensB <vivens.byiringiro77@gmail.com>
2024-10-14 13:28:38 +0200

    Added db migration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,63 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 dotenv.config();
 
 const app = express();
+
+// Enable CORS for requests from localhost:3000
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 4500;
 
 app.get("/", (req, res) => {
   res.send("Hello from Express server!");
+});
+
+app.post("/add-employee", async (req, res) => {
+  try {
+    const { firstName, lastname, email, gender, phone_number, dateOfBirth } =
+      req.body;
+    if (
+      !firstName ||
+      !lastname ||
+      !email ||
+      !gender ||
+      !phone_number ||
+      !dateOfBirth
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const employee = await prisma.employee.create({
+      data: {
+        firstName,
+        lastname,
+        email,
+        gender,
+        phone_number,
+        dateOfBirth: new Date(dateOfBirth),
+      },
+    });
+    res.status(201).json({ success: true, employee });
+  } catch (error) {
+    console.error("Error adding employee:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+app.get("/employees", async (req, res) => {
+  try {
+    const employees = await prisma.employee.findMany();
+    res.status(200).json({ success: true, employees });
+  } catch (error) {
+    console.error("Error fetching employees:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import moment from "moment";
 function App() {
+  const [employees, setEmployees] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("http://localhost:4500/employees");
+        setEmployees(response.data.employees);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchEmployees();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table className="table table-stripped table-hover mt-5">
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Date Of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastname}</td>
+              <td>{employee.email}</td>
+              <td>{employee.gender}</td>
+              <td>{employee.phone_number}</td>
+              <td>{moment(employee.dateOfBirth).format("DD/MM/YYYY")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
